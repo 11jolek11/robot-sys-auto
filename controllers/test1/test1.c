89452ba77e6b7628cc1f0654e13100472291f7ac
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-06-01 18:23:31 +0200

    renamed macros

--- controllers/test1/test1.c
+++ controllers/test1/test1.c
@@ -8,12 +8,12 @@
 
 // Define the distance sensors' indices
 #define NUM_SENSORS 8
-#define LEFT_SENSOR 0
-#define LEFT_FRONT_SENSOR 1
-#define FRONT_LEFT_SENSOR 2
-#define FRONT_RIGHT_SENSOR 5
-#define RIGHT_FRONT_SENSOR 6
-#define RIGHT_SENSOR 7
+#define RIGHT_SENSOR 0
+#define RIGHT_FRONT_SENSOR 1
+#define FRONT_RIGHT_SENSOR 2
+#define FRONT_LEFT_SENSOR 5
+#define LEFT_FRONT_SENSOR 6
+#define LEFT_SENSOR 7
 
 int main(int argc, char **argv) {
   wb_robot_init();
@@ -45,24 +45,24 @@ int main(int argc, char **argv) {
     double left_speed = MAX_SPEED * 0.5;
     double right_speed = MAX_SPEED * 0.5;
 
-    if (sensor_values[RIGHT_SENSOR] > 80 || sensor_values[RIGHT_FRONT_SENSOR] > 80) {
-      // Turn left if there's a wall on the right
+    if (sensor_values[LEFT_SENSOR] > 80 || sensor_values[LEFT_FRONT_SENSOR] > 80) {
+      // Turn RIGHT if there's a wall on the LEFT
       left_speed += 0.5 * MAX_SPEED;
       right_speed -= 0.5 * MAX_SPEED;
-    } else if (sensor_values[LEFT_SENSOR] > 80 || sensor_values[LEFT_FRONT_SENSOR] > 80) {
-      // Turn right if there's a wall on the left
+    } else if (sensor_values[RIGHT_SENSOR] > 80 || sensor_values[RIGHT_FRONT_SENSOR] > 80) {
+      // Turn LEFT if there's a wall on the RIGHT
       left_speed -= 0.5 * MAX_SPEED;
       right_speed += 0.5 * MAX_SPEED;
-    } else if (sensor_values[FRONT_LEFT_SENSOR] > 80 || sensor_values[FRONT_RIGHT_SENSOR] > 80) {
+    } else if (sensor_values[FRONT_RIGHT_SENSOR] > 80 || sensor_values[FRONT_LEFT_SENSOR] > 80) {
       // Turn around if there's a wall in front
       left_speed = MAX_SPEED;
       right_speed = -MAX_SPEED;
-    } else if (sensor_values[LEFT_SENSOR] > 60 && sensor_values[RIGHT_SENSOR] > 1000) {
+    } else if (sensor_values[RIGHT_SENSOR] > 60 && sensor_values[LEFT_SENSOR] > 1000) {
       // Turn around if there's a wall in front
       printf("TICK\n");
     }
 
-    printf("%g -- %g * %g -- %g \n", sensor_values[LEFT_FRONT_SENSOR], sensor_values[LEFT_SENSOR], sensor_values[RIGHT_SENSOR], sensor_values[RIGHT_FRONT_SENSOR]);
+    printf("%g -- %g * %g -- %g \n", sensor_values[RIGHT_FRONT_SENSOR], sensor_values[RIGHT_SENSOR], sensor_values[LEFT_SENSOR], sensor_values[LEFT_FRONT_SENSOR]);
     // Set motor speeds
     wb_motor_set_velocity(left_motor, left_speed);
     wb_motor_set_velocity(right_motor, right_speed);
