9aafb11fd8bdccf8374d7468cd8c23fa17cd8ff2
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-06-02 15:43:48 +0200

    rewrite of test.c

--- controllers/test1/test1.c
+++ controllers/test1/test1.c
@@ -31,84 +31,84 @@
 
 // static double old_delta_orientation = 0.0;
 
-// // void format_date_time(char *output){
-// //     time_t rawtime;
-// //     struct tm * timeinfo;
+// void format_date_time(char *output){
+//     time_t rawtime;
+//     struct tm * timeinfo;
     
-// //     time(&rawtime);
-// //     timeinfo = localtime(&rawtime);
+//     time(&rawtime);
+//     timeinfo = localtime(&rawtime);
     
-// //     sprintf(output, "%d-%d-%d %d:%d:%d", timeinfo->tm_mday,
-// //             timeinfo->tm_mon + 1, timeinfo->tm_year + 1900,
-// //             timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-// // }
+//     sprintf(output, "%d-%d-%d %d:%d:%d", timeinfo->tm_mday,
+//             timeinfo->tm_mon + 1, timeinfo->tm_year + 1900,
+//             timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+// }
 
-// // void format_date_with_sec(char *output){
-// //     time_t rawtime;
-// //     struct tm * timeinfo;
+// void format_date_with_sec(char *output){
+//     time_t rawtime;
+//     struct tm * timeinfo;
     
-// //     time(&rawtime);
-// //     timeinfo = localtime(&rawtime);
+//     time(&rawtime);
+//     timeinfo = localtime(&rawtime);
     
-// //     sprintf(output, "%d_%d_%d_%d_%d_%d", 
-// //       timeinfo->tm_mday,
-// //       timeinfo->tm_mon + 1, timeinfo->tm_year + 1900,
-// //       timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-// // }
+//     sprintf(output, "%d_%d_%d_%d_%d_%d", 
+//       timeinfo->tm_mday,
+//       timeinfo->tm_mon + 1, timeinfo->tm_year + 1900,
+//       timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+// }
 
-// // void get_center_of_camera(int *buffer, int camera_width, int camera_height) {
-// //   buffer[0] = camera_width/2;
-// //   printf("Buff %d \n", buffer[0]);
-// //   buffer[1] = camera_height/2;
-// // }
+// void get_center_of_camera(int *buffer, int camera_width, int camera_height) {
+//   buffer[0] = camera_width/2;
+//   printf("Buff %d \n", buffer[0]);
+//   buffer[1] = camera_height/2;
+// }
 
-// // int get_for_color_in_point(const unsigned char *image, int width, int height, char *sink) {
-// //   int r = wb_camera_image_get_red(image, camera_width, width, height);
-// //   int g = wb_camera_image_get_green(image, camera_width, width, height);
-// //   int b = wb_camera_image_get_blue(image, camera_width, width, height);
+// int get_for_color_in_point(const unsigned char *image, int width, int height, char *sink) {
+//   int r = wb_camera_image_get_red(image, camera_width, width, height);
+//   int g = wb_camera_image_get_green(image, camera_width, width, height);
+//   int b = wb_camera_image_get_blue(image, camera_width, width, height);
   
-// //   char timestamp[50] = {0};
-// //   format_date_time(timestamp);
+//   char timestamp[50] = {0};
+//   format_date_time(timestamp);
   
-// //   // fprintf(stdin, "[%s], r=%d, g=%d, b=%d\n", timestamp, r, g, b);
-// //   if (sink != NULL) { // r g b
-// //     char lol[32] = {0};
-// //     sprintf(lol, "%d,%d,%d,", r, g, b);
-// //     strcat(sink, lol);  
-// //   }
+//   // fprintf(stdin, "[%s], r=%d, g=%d, b=%d\n", timestamp, r, g, b);
+//   if (sink != NULL) { // r g b
+//     char lol[32] = {0};
+//     sprintf(lol, "%d,%d,%d,", r, g, b);
+//     strcat(sink, lol);  
+//   }
 
-// //   return 0;
-// // }
+//   return 0;
+// }
 
-// // double angle_diff(WbDeviceTag left_position_sensor, WbDeviceTag right_position_sensor) {
-// //   double da = 0.0;
-// //   double l = wb_position_sensor_get_value(left_position_sensor);
-// //   double r = wb_position_sensor_get_value(right_position_sensor);
-// //   double dl = l * WHEEL_RADIUS;         // distance covered by left wheel in meter
-// //   double dr = r * WHEEL_RADIUS;         // distance covered by right wheel in meter
-// //   da = (dr - dl) / AXLE_LENGTH;  // delta orientation
-// //   // printf("estimated distance covered by left wheel: %g m.\n", dl);
-// //   // printf("estimated distance covered by right wheel: %g m.\n", dr);
-// //   // printf("estimated orientation: %g rad.\n", da);
-// //   double delta = da - old_delta_orientation;
-// //   // printf("estimated change of orientation: %g rad.\n", delta);
-// //   old_delta_orientation = da;
-// //   return delta;
-// // }
+// double angle_diff(WbDeviceTag left_position_sensor, WbDeviceTag right_position_sensor) {
+//   double da = 0.0;
+//   double l = wb_position_sensor_get_value(left_position_sensor);
+//   double r = wb_position_sensor_get_value(right_position_sensor);
+//   double dl = l * WHEEL_RADIUS;         // distance covered by left wheel in meter
+//   double dr = r * WHEEL_RADIUS;         // distance covered by right wheel in meter
+//   da = (dr - dl) / AXLE_LENGTH;  // delta orientation
+//   // printf("estimated distance covered by left wheel: %g m.\n", dl);
+//   // printf("estimated distance covered by right wheel: %g m.\n", dr);
+//   // printf("estimated orientation: %g rad.\n", da);
+//   double delta = da - old_delta_orientation;
+//   // printf("estimated change of orientation: %g rad.\n", delta);
+//   old_delta_orientation = da;
+//   return delta;
+// }
 
 // int main(int argc, char **argv) {
-//   // char curr_date[50] = {0};
-//   // format_date_with_sec(curr_date); 
-//   // char file_path[100] = "./DIST_LOGS_";
-//   // strcat(file_path, curr_date);
-//   // strcat(file_path, ".csv");
-//   // printf("%s \n", file_path);
+//   char curr_date[50] = {0};
+//   format_date_with_sec(curr_date); 
+//   char file_path[100] = "./DIST_LOGS_";
+//   strcat(file_path, curr_date);
+//   strcat(file_path, ".csv");
+//   printf("%s \n", file_path);
 
-//   // FILE *header = fopen(file_path, "w+");
-//   // fprintf(header, "time,ps0,ps1,ps2,ps3,ps4,ps5,ps6,ps7,rotation,r,g,b,\n");
-//   // fclose(header);
+//   FILE *header = fopen(file_path, "w+");
+//   fprintf(header, "time,ps0,ps1,ps2,ps3,ps4,ps5,ps6,ps7,rotation,r,g,b,\n");
+//   fclose(header);
 
-//   // FILE *logs = fopen(file_path, "a");
+//   FILE *logs = fopen(file_path, "a");
 
 //   int time_step;
 //   int camera_time_step;
@@ -138,12 +138,12 @@
 //   wb_position_sensor_enable(left_position_sensor, time_step);
 //   wb_position_sensor_enable(right_position_sensor, time_step);
 
-//   // camera = wb_robot_get_device("camera");
-//   // wb_camera_enable(camera, camera_time_step);
+//   camera = wb_robot_get_device("camera");
+//   wb_camera_enable(camera, camera_time_step);
   
-//   // camera_width = wb_camera_get_width(camera);
-//   // camera_height = wb_camera_get_height(camera);
-//   // get_center_of_camera(center, camera_width, camera_height);
+//   camera_width = wb_camera_get_width(camera);
+//   camera_height = wb_camera_get_height(camera);
+//   get_center_of_camera(center, camera_width, camera_height);
 
 //   // accelerometer = wb_robot_get_device("accelerometer");
 //   // wb_accelerometer_enable(accelerometer, time_step);
@@ -162,10 +162,10 @@
 //   }
 
 //   while (wb_robot_step(TIME_STEP) != -1) {
-//     // char log_entry[512] = {0};
-//     // double angle = angle_diff(left_position_sensor, right_position_sensor);
+//     char log_entry[512] = {0};
+//     double angle = angle_diff(left_position_sensor, right_position_sensor);
 
-//     // const unsigned char *image = wb_camera_get_image(camera);
+//     const unsigned char *image = wb_camera_get_image(camera);
 
 //     // Read sensor values
 //     double sensor_values[NUM_SENSORS];
@@ -215,28 +215,28 @@
 
 //     // "time,ps0,ps1,ps2,ps3,ps4,ps5,ps6,ps7,rotation,r,g,b,left_speed,right_speed\n"
 
-//     // char timestamp[50] = {0};
-//     // format_date_time(timestamp);
-//     // strcat(log_entry, timestamp);
-//     // FILE *logs = fopen(file_path, "a");
-//     // fprintf(logs, "%s,", timestamp);
+//     char timestamp[50] = {0};
+//     format_date_time(timestamp);
+//     strcat(log_entry, timestamp);
+//     FILE *logs = fopen(file_path, "a");
+//     fprintf(logs, "%s,", timestamp);
 
-//     // for (int i = 0; i <= 7; i++) {
-//     //   char ttemp[32] = {0};
-//     //   sprintf(ttemp, "%f,", sensor_values[i]);
-//     //   strcat(log_entry, ttemp);
-//     //  }
+//     for (int i = 0; i <= 7; i++) {
+//       char ttemp[32] = {0};
+//       sprintf(ttemp, "%f,", sensor_values[i]);
+//       strcat(log_entry, ttemp);
+//      }
 
-//     // char tempp[32] = {0};
-//     // sprintf(tempp, "%f,", angle);
-//     // strcat(log_entry, tempp);
+//     char tempp[32] = {0};
+//     sprintf(tempp, "%f,", angle);
+//     strcat(log_entry, tempp);
 
-//     // char temp[64] = {0};
-//     // get_for_color_in_point(image, center[0], center[1], temp);
-//     // strcat(log_entry, temp);
-//     // strcat(log_entry, "\n");
+//     char temp[64] = {0};
+//     get_for_color_in_point(image, center[0], center[1], temp);
+//     strcat(log_entry, temp);
+//     strcat(log_entry, "\n");
 
-//     // printf(log_entry);
+//     printf(log_entry);
 
 //     // Set motor speeds
 //     wb_motor_set_velocity(left_motor, left_speed);
@@ -244,15 +244,18 @@
 
 //   }
 
-//   // fclose(logs);
+//   fclose(logs);
 //   wb_robot_cleanup();
 //   return 0;
 // }
 
+// ######################################################################################################33
 
 #include <webots/robot.h>
 #include <webots/motor.h>
 #include <webots/distance_sensor.h>
+#include <webots/camera.h>
+#include <string.h>
 #include <stdio.h>
 
 #define TIME_STEP 64
@@ -268,15 +271,93 @@
 #define RIGHT_SENSOR 7
 
 // Initialize motors
-WbDeviceTag left_motor, right_motor;
+WbDeviceTag left_motor, right_motor, camera, sensors[NUM_SENSORS];
 
 void halt() {
   wb_motor_set_velocity(left_motor, 0.0);
   wb_motor_set_velocity(right_motor, 0.0);
 }
+// 52 x 39
+int points_of_interests[5][2] = {
+  {13, 13},
+  {13, 26},
+  {39, 13},
+  {39, 26},
+
+  {26, 19}
+};
+
+static int camera_width;
+static int camera_height;
+
+int check_for_color_in_point(const unsigned char *image, int width, int height) {
+  int r = wb_camera_image_get_red(image, camera_width, width, height);
+  int g = wb_camera_image_get_green(image, camera_width, width, height);
+  int b = wb_camera_image_get_blue(image, camera_width, width, height);
+  printf("r%dg%db%d at %dx%d \n", r, g, b, width, height);
+
+// (r >= 63 && r <= 69) && (r >= 200 && r <= 210) &&
+// (g >= 58 && g <= 64) && (g >= 178 && g <= 188) &&
+// (b >= 20 && b <= 30) && (b >= 35 && b <= 42)
+
+// g >= 58 && g <= 64
+// g >= 178 && g <= 188
+
+// b >= 20 && b <= 30
+// b >= 35 && b <= 42
+
+  if (
+  // ((r >= 63 && r <= 69) || (r >= 200 && r <= 210)) &&
+  // ((g >= 58 && g <= 64) || (g >= 178 && g <= 188)) &&
+  // ((b >= 20 && b <= 30) || (b >= 35 && b <= 42))
+
+  // ((r >= 210 && r <= 255)) &&
+  // ((g >= 230 && g <= 245)) &&
+  // ((b >= 0 && b <= 58))
+
+  ((r >= 64 && r <= 66)) &&
+  ((g >= 59 && g <= 61)) &&
+  ((b >= 21 && b <= 27))
+
+  // orange and blue
+  // ((r >= 63 && r <= 69) || (r >= 200 && r <= 210)) &&
+  // ((g >= 58 && g <= 64) || (g >= 178 && g <= 188)) &&
+  // ((b >= 20 && b <= 30) || (b >= 35 && b <= 42))
+
+  ) {
+    printf("FOUND MATCHING COLOR!!!: red=%d, green=%d, blue=%d \n", r, g, b);
+    return 1;
+  }
+
+  return 0;
+}
 
 int main(int argc, char **argv) {
   wb_robot_init();
+
+  int time_step;
+  int camera_time_step;
+  
+  /* initialize Webots */
+  wb_robot_init();
+
+  if (strcmp(wb_robot_get_model(), "GCtronic e-puck2") == 0) {
+    printf("e-puck2 robot\n");
+    time_step = 64;
+    camera_time_step = 64;
+  } else {  // original e-puck
+    printf("e-puck robot\n");
+    time_step = 64;
+    camera_time_step = 1024;
+  }
+
+  camera = wb_robot_get_device("camera");
+  wb_camera_enable(camera, camera_time_step);
+  camera_width = wb_camera_get_width(camera);
+  camera_height = wb_camera_get_height(camera);
+
+  printf("Camera: %d x %d \n", camera_width, camera_height);
+  
 
   left_motor = wb_robot_get_device("left wheel motor");
   right_motor = wb_robot_get_device("right wheel motor");
@@ -285,9 +366,7 @@ int main(int argc, char **argv) {
   wb_motor_set_velocity(left_motor, 0.0);
   wb_motor_set_velocity(right_motor, 0.0);
 
-
   // Initialize distance sensors
-  WbDeviceTag sensors[NUM_SENSORS];
   char sensor_names[NUM_SENSORS][5] = {"ps0", "ps1", "ps2", "ps3", "ps4", "ps5", "ps6", "ps7"};
   for (int i = 0; i < NUM_SENSORS; i++) {
     sensors[i] = wb_robot_get_device(sensor_names[i]);
@@ -295,6 +374,25 @@ int main(int argc, char **argv) {
   }
 
   while (wb_robot_step(TIME_STEP) != -1) {
+    const unsigned char *image = wb_camera_get_image(camera);
+
+    // int i, j;
+    // for (i = 0; i < 5; i++) {
+    //   for (j = 0; j < 2; j++) {
+    //     printf("* %d\n", points_of_interests[i][j]);
+    //   }
+    // }
+
+    for (int i = 0; i <= 4; i++) {
+      // printf("%d x %d \n",  points_of_interests[i][0], points_of_interests[i][1]);
+      int detected = check_for_color_in_point(image, points_of_interests[i][0], points_of_interests[i][1]);
+      if (detected) {
+        halt();
+        wb_robot_cleanup();
+        return 0;
+      }
+    }
+
     // Read sensor values
     double sensor_values[NUM_SENSORS];
     for (int i = 0; i < NUM_SENSORS; i++) {
@@ -310,7 +408,7 @@ int main(int argc, char **argv) {
         (sensor_values[LEFT_SENSOR] >= 100) && 
         (sensor_values[LEFT_FRONT_SENSOR] >= 100)) {
                
-                    printf("HELLO \n");
+                    // printf("HELLO \n");
 
                     left_speed = -MAX_SPEED;
                     right_speed = -MAX_SPEED;   
@@ -320,8 +418,8 @@ int main(int argc, char **argv) {
                     wb_robot_step(256);
                     
                     
-                    left_speed = -0.5*MAX_SPEED;
-                    right_speed = 0.5*MAX_SPEED;   
+                    left_speed = 0.5*MAX_SPEED;
+                    right_speed = -0.5*MAX_SPEED;   
                     wb_motor_set_velocity(left_motor, left_speed);
                     wb_motor_set_velocity(right_motor, right_speed);
                     wb_robot_step(256);
